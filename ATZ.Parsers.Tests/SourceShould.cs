00d52dbf90dd24f1df88f7153202608750708f20
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-18 20:43:30 +1000

    Initial checkin.

---
+++ ATZ.Parsers.Tests/SourceShould.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ATZ.Parsers.Tests
+{
+    [TestFixture]
+    public class SourceShould
+    {
+        [Test]
+        public void BeConstructable()
+        {
+            using (var sr = new StringReader("X"))
+            {
+                // ReSharper disable once ObjectCreationAsStatement => We are testing the constructor against exceptions.
+                Assert.DoesNotThrow(() => new Source(sr));
+            }
+        }
+
+        [Test]
+        public void ReturnTheCurrentCharacterOnInitialCallToCurrentCharacter()
+        {
+            using (var sr = new StringReader("X"))
+            {
+                var s = new Source(sr);
+                s.CurrentCharacter.Should().Be('X');
+            }
+        }
+
+        [Test]
+        public void ReturnEndOfFileOnCurrentCharacterIfTheStreamIsEmpty()
+        {
+            using (var sr = new StringReader(""))
+            {
+                var s = new Source(sr);
+                s.CurrentCharacter.Should().Be(Source.Eof);
+            }
+        }
+
+        [Test]
+        public void ReturnEndOfLineOnCurrentCharacterIfTheLineIsAtItsEnd()
+        {
+            using (var sr = new StringReader("\n"))
+            {
+                var s = new Source(sr);
+                s.CurrentCharacter.Should().Be(Source.Eol);
+            }
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionWhenPassingNullAsConstructorParameter()
+        {
+            // ReSharper disable once ObjectCreationAsStatement => We are testing the constructor.
+            var ex = Assert.Throws<ArgumentNullException>(() => new Source(null));
+            ex.Should().NotBeNull();
+            ex.ParamName.Should().Be("textReader");
+        }
+
+        [Test]
+        public void ReturnEndOfFileWhenAdvancingAfterTheEndOfFile()
+        {
+            using (var sr = new StringReader(""))
+            {
+                var s = new Source(sr);
+                s.NextCharacter();
+                s.CurrentCharacter.Should().Be(Source.Eof);
+            }
+        }
+
+        [Test]
+        public void AdvanceLinePositionWhenReadingPastTheEndOfLine()
+        {
+            using (var sr = new StringReader("\nX"))
+            {
+                var s = new Source(sr);
+                s.CurrentCharacter.Should().Be(Source.Eol);
+                s.NextCharacter();
+                s.CurrentCharacter.Should().Be('X');
+            }
+        }
+
+        [Test]
+        public void ReturnEndOfFileWhenPeekingEmptyFile()
+        {
+            using (var sr = new StringReader(""))
+            {
+                var s = new Source(sr);
+                s.PeekCharacter().Should().Be(Source.Eof);
+            }
+        }
+
+        [Test]
+        public void ReturnEndOfLineWhenPeekingAtTheEndOfLine()
+        {
+            using (var sr = new StringReader("\nX"))
+            {
+                var s = new Source(sr);
+                s.PeekCharacter().Should().Be(Source.Eol);
+            }
+        }
+
+        [Test]
+        public void ReturnNextCharacterWhenPeekingInTheMiddleOfTheLine()
+        {
+            using (var sr = new StringReader("AB"))
+            {
+                var s = new Source(sr);
+                s.PeekCharacter().Should().Be('B');
+            }
+        }
+
+        [Test]
+        public void ReturnZeroForLineNumberIfSourceIsEmpty()
+        {
+            using (var sr = new StringReader(""))
+            {
+                var s = new Source(sr);
+                s.LineNumber.Should().Be(0);
+            }
+        }
+
+        [Test]
+        public void ReturnOneForLineNumberOfAnyNonEmptySourceWithoutAdvancingTheCharacter()
+        {
+            using (var sr = new StringReader("A"))
+            {
+                var s = new Source(sr);
+                s.LineNumber.Should().Be(1);
+            }
+        }
+
+        [Test]
+        public void IncrementLineNumberWhenPassingNewLines()
+        {
+            using (var sr = new StringReader("\nA"))
+            {
+                var s = new Source(sr);
+                s.LineNumber.Should().Be(1);
+                s.NextCharacter();
+                s.LineNumber.Should().Be(2);
+            }
+        }
+    }
+}
---
+++ ATZ.Parsers/Source.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace ATZ.Parsers
+{
+    public class Source
+    {
+        public const char Eol = '\n';
+        public const char Eof = (char)0;
+
+        [NotNull]
+        private readonly TextReader _textReader;
+        private string _line;
+
+        public int CurrentPosition { get; private set; } = -2;
+        public char CurrentCharacter { get; private set; }
+        public int LineNumber { get; private set; } = 0;
+
+        public Source([NotNull] TextReader textReader)
+        {
+            _textReader = textReader ?? throw new ArgumentNullException(nameof(textReader));
+            ReadNextLine();
+            NextCharacter();
+        }
+
+        private void IncrementCurrentPosition()
+        {
+            ++CurrentPosition;
+            if (_line == null || CurrentPosition <= _line.Length)
+            {
+                return;
+            }
+
+            ReadNextLine();
+            ++CurrentPosition;
+        }
+
+        private void ReadCurrentCharacter()
+        {
+            if (_line == null)
+            {
+                CurrentCharacter = Eof;
+            }
+            else if (CurrentPosition == -1 || CurrentPosition == _line.Length)
+            {
+                CurrentCharacter = Eol;
+            }
+            else
+            {
+                CurrentCharacter = _line[CurrentPosition];
+            }
+        }
+
+        private void ReadNextLine()
+        {
+            _line = _textReader.ReadLine();
+            CurrentPosition = -1;
+            if (_line != null)
+            {
+                ++LineNumber;
+            }
+        }
+
+        public void NextCharacter()
+        {
+            IncrementCurrentPosition();
+            ReadCurrentCharacter();
+        }
+
+        public char PeekCharacter()
+        {
+            if (_line == null)
+            {
+                return Eof;
+            }
+            int nextPos = CurrentPosition + 1;
+            return nextPos < _line.Length ? _line[nextPos] : Eol;
+        }
+
+    }
+}
