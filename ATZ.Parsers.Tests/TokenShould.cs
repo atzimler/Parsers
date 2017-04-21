4d147410ed83833a023561f3e4d43018cb2dad78
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-22 09:20:32 +1000

    Code cleanup.

--- ATZ.Parsers.Tests/TokenShould.cs
+++ ATZ.Parsers.Tests/TokenShould.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using FluentAssertions;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using FluentAssertions;
-using JetBrains.Annotations;
-using NUnit.Framework;
 
 namespace ATZ.Parsers.Tests
 {
